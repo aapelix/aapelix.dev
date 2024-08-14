c25a3f37ca36ec2b1126d199bc8fb63ac8a17444
aapelix <mcaapelix@gmail.com>
2024-08-14 19:12:20 +0300

    Committed using Argonium

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,96 +78,20 @@ export default function Home() {
   ];
 
   return (
-    <>
-      <div className="flex h-screen flex-wrap items-center justify-center gap-x-44">
-        <h1 className="text-2xl text-zinc-400">
-          <motion.p
-            animate={{ y: 0, opacity: 100 }}
-            initial={{ y: 20, opacity: 0 }}
-            transition={{ delay: 1.4 }}
-          >
-            It{"'"}s me,
-            <span className="text-zinc-300 font-semibold"> Aapelix</span>.
-          </motion.p>
-          <motion.p
-            animate={{ y: 0, opacity: 100 }}
-            initial={{ y: 20, opacity: 0 }}
-            transition={{ delay: 1.6 }}
-          >
-            I{"'"}m a
-            <span className="text-zinc-300 font-semibold"> programmer</span>{" "}
-            working on
-          </motion.p>
-          <motion.p
-            animate={{ y: 0, opacity: 100 }}
-            initial={{ y: 20, opacity: 0 }}
-            transition={{ delay: 1.8 }}
-          >
-            my own projects for fun
-          </motion.p>
-        </h1>
-        <motion.svg
-          width="617"
-          height="809"
-          viewBox="0 0 617 809"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="px-5"
-        >
-          <motion.path
-            d="M313.5 808L507.5 3.5M507.5 3.5L217.5 808M507.5 3.5L126.5 808M507.5 3.5L407 808M507.5 3.5L522.5 808M507.5 3.5L616 808M507.5 3.5L11 808"
-            stroke="white"
-            strokeWidth="2"
-            initial={{ pathLength: 0 }}
-            animate={{ pathLength: 1 }}
-            transition={{
-              duration: 3,
-              ease: "easeInOut",
-            }}
-          />
-          <motion.path
-            d="M488.526 296.2C472.918 419.627 374.567 518 247 518C111.138 518 1 407.862 1 272C1 136.138 111.138 26 247 26C309.873 26 341.524 71.8443 369.345 124.767C373.634 132.925 377.834 141.254 382.042 149.602C391.445 168.253 400.892 186.992 411.482 204.105C426.827 228.901 444.664 250.45 468.469 263.618C480.815 270.447 490.25 282.566 488.526 296.2Z"
-            stroke="white"
-            strokeWidth="2"
-            initial={{ pathLength: 0 }}
-            animate={{ pathLength: 1 }}
-            transition={{
-              duration: 1,
-              ease: "easeInOut",
-            }}
-          />
-        </motion.svg>
+    <div className="w-full h-screen flex justify-center items-center text-white flex-col gap-y-4">
+      <h1 className="text-6xl font-black">It's me, aapelix</h1>
+      <div className="flex flex-wrap gap-3 md:flex-row flex-col justify-center items-center">
+        <a href="https://github.com/aapelix">
+          <button className="w-44 hover:w-96 duration-300 h-12 bg-white text-black rounded-full">
+            Github
+          </button>
+        </a>
+        <a href="https://weather.aapelix.dev">
+          <button className="w-44 hover:w-96 duration-300 h-12 bg-white text-black rounded-full">
+            Weather
+          </button>
+        </a>
       </div>
-      <div className="flex justify-center flex-col w-screen pb-4">
-        <p className="text-center text-zinc-400 mb-3">
-          Section working progress
-        </p>
-        <div className="grid grid-cols-3 gap-3 mx-10">
-          {items.map((item, index) => (
-            <div
-              className={
-                "bg-[#1a1a1a] text-zinc-300 h-[30rem] p-4 flex flex-col rounded-3xl items-start relative hover:scale-[1.03] hover:shadow-xl duration-300" +
-                " " +
-                item.className
-              }
-              key={index}
-            >
-              {item.header}
-              <h1 className="mt-3 text-lg font-bold">{item.title}</h1>
-              <p className="text-zinc-400">{item.description}</p>
-              <p className="text-zinc-400 absolute left-4 bottom-5">
-                {item.published}
-              </p>
-              <a
-                href={item.url}
-                className="absolute bottom-5 right-5 hover:scale-105 duration-300"
-              >
-                <SquareArrowOutUpRight />
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
